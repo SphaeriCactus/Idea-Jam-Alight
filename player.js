4af3a1cb93f9b34f03b641483e177924df7ad495
SphaeriCactus <sphaericactus@gmail.com>
2018-10-27 16:33:05 -0700

    Update

--- player.js
+++ player.js
@@ -17,9 +17,9 @@ Player.prototype.ableToJump = function() {
 };
 
 Player.prototype.update = function() {
-	if ((keys[LEFT_ARROW] || keys[65]) && this.x - this.s/2 >= this.speed) {
+	if ((keys[LEFT_ARROW] || keys[65]) && this.x - this.s/2 >= this.speed && !this.jumping) {
 		this.x -= this.speed;
-	} else if ((keys[RIGHT_ARROW] || keys[68]) && this.x + this.s/2 <= (width - this.speed)) {
+	} else if ((keys[RIGHT_ARROW] || keys[68]) && this.x + this.s/2 <= (width - this.speed) && !this.jumping) {
 		this.x += this.speed;
 	}
 
